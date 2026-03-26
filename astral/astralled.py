6f1663b8e3365834a707b97f2df26b7a6af5615a
CaptainKirky <32111167+CaptainKirky@users.noreply.github.com>
2026-03-26 18:18:48 +0000

    Updates to new library
    New library compatible with Pi5

--- astral/astralled.py
+++ astral/astralled.py
@@ -2,14 +2,17 @@ from datetime import datetime
 from astral import LocationInfo
 from astral.sun import sun
 import pytz
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO # Old library not compatible with Pi5
+import lgpio
 import time
 
-# SET GPIO PINS
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(13, GPIO.OUT)
+GPIO_LED_PIN = 13
 
+# Open GPIO chip (0 is usually correct for Raspberry Pi)
+chip = lgpio.gpiochip_open(0)
+
+# Claim the pin as output
+lgpio.gpio_claim_output(chip, GPIO_LED_PIN)
 
 def is_sun_down(latitude, longitude, timezone):
     city = LocationInfo(latitude=latitude, longitude=longitude, timezone=timezone)
@@ -20,9 +23,11 @@ def is_sun_down(latitude, longitude, timezone):
 
 while True:
     if is_sun_down(51.379608, -0.243710, "Europe/London"):
-        #print("🌙 Sun is down")
-        GPIO.output(13, GPIO.HIGH)
+        print("🌙 Sun is down")
+        lgpio.gpio_write(chip, GPIO_LED_PIN, 1)  # LED ON
     else:
-        #print("☀️ Sun is up")
-        GPIO.output(13, GPIO.LOW)
+        print("☀️ Sun is up")
+        lgpio.gpio_write(chip, GPIO_LED_PIN, 0)  # LED OFF
     time.sleep(5)
+
+lgpio.gpiochip_close(chip)
